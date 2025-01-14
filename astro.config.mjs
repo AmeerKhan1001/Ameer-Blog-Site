77e15056f90b31f4f3bd3b68564bcb14cd202ad9
Ameer Khan <ameerkhan12@gmail.com>
2025-01-14 20:21:59 +0530

    Added other libraries

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,26 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
+import icon from "astro-icon";
+
+import svelte from "@astrojs/svelte";
+
+import react from "@astrojs/react";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://example.com",
+  integrations: [
+  preact({
+    include: ['**/preact/*'],
+  }),
+  react({
+    include: ['**/react/*'],
+  }),
+  svelte({
+    include: ['**/svelte/*'],
+  }),
+  icon()]
+});
---
+++ src/components/svelte/SvelteCounter.svelte
@@ -0,0 +1,17 @@
+<script>
+  let count = 0;
+
+  const increment = () => {
+    count += 1;
+  };
+
+  const decrement = () => {
+    count -= 1;
+  };
+</script>
+
+<div>
+  <h1>Svelte Counter: {count}</h1>
+  <button on:click={decrement}>-</button>
+  <button on:click={increment}>+</button>
+</div>
